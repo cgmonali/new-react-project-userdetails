a2abcf072da3ae10ae7ae26cd566a2aaaa0f254f
Monali C G <cgmonali@gmail.com>
2023-07-05 10:41:19 +0530

    used Context API in-_a__login_project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import AuthContext from './store/auth-Context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -29,11 +29,15 @@ function App() {
 
   return (
     <React.Fragment>
+      <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn
+      }}  >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
